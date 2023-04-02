595b32ae56f8af0dddd41855a59b24a6889e00fb
hykare <78385964+hykare@users.noreply.github.com>
2023-04-02 22:56:18 +0200

    allow remote access to localhost server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "6218-5-184-235-151.eu.ngrok.io"
 end
