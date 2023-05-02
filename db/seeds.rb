80b38246d3104d47a67a7a214be08bd086aec270
hykare <78385964+hykare@users.noreply.github.com>
2023-05-02 20:37:33 +0200

    seed appoitments

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,31 @@
 
 # === create patients
 
-5.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = "#{first_name}_#{last_name}@email.com"
-  password = '123123'
+# 50.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   email = "#{first_name}_#{last_name}@email.com"
+#   password = '123123'
 
-  Patient.create(email:, password:, first_name:, last_name:)
+#   Patient.create(email:, password:, first_name:, last_name:)
+# end
+
+# === create appointments
+Appointment.delete_all
+
+Doctor.all.each do |doctor|
+  duration = 15.minutes
+
+  5.times do |i|
+    patient = Patient.find(Patient.ids.sample)
+    # might be faster
+    # patient_count = Patient.count
+    # random_offset = rand(patient_count)
+    # random_patient = Patient.offset(random_offset).first
+
+    shift_start = Date.today + 8.hours
+    appointment_time = shift_start + i * duration
+
+    Appointment.create(doctor:, patient:, start_time: appointment_time, duration:)
+  end
 end
