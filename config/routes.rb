35b483c42843f113edcbed1989de769401154f91
hykare <78385964+hykare@users.noreply.github.com>
2023-05-05 11:02:03 +0200

    add navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'doctors#index'
+
   devise_for :patients
   devise_for :doctors, defaults: { format: :json }, controllers: {
     sessions: 'doctors/sessions',
@@ -8,4 +10,5 @@ Rails.application.routes.draw do
   resources :specialties, only: [:index]
   resources :appointments, only: [:index]
   resources :doctors, only: [:index, :show]
+  resources :patients, only: [:show, :edit]
 end
