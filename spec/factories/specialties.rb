2b66cdc197fee57e595dced970a0c8702d95d44c
hykare <78385964+hykare@users.noreply.github.com>
2023-05-22 19:59:29 +0200

    specialties factory for testing

--- spec/factories/specialties.rb
+++ spec/factories/specialties.rb
@@ -1,5 +1,16 @@
+specialties = %w[
+  ginekolog
+  psycholog
+  ortopeda
+  stomatolog
+  psychiatra
+  chirurg
+  laryngolog
+  dermatolog
+]
+
 FactoryBot.define do
   factory :specialty do
-    
+    name { specialties.sample }
   end
 end
